0871bc3ea4f88adddd62f8d333718239ac7a1be9
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-05-15 09:13:12 +0530

    added day 11

---
+++ day11.js
@@ -0,0 +1,38 @@
+/*
+Introduction to asynchronous programming
+Promises - a way to handle asynchronous operations. All asynchronous operations will return a promise object.
+states of a promise: pending, resolved, rejected    
+
+function callback(resolve, reject) {
+    do network request or any other operations that take time
+
+    eg.
+    setTimeout(() => {
+        resolve('success');
+    }, 2000);
+}
+
+const promise = new Promise(callback);
+promise.then((result) => {
+    now we can use the result
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
+*/
+
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, millis);
+    });
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
