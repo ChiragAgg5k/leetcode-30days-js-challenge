fa55bed2a79bdfd7fe9e7162c378f0785e0d0f20
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-05-22 20:43:47 +0530

    added day 15 and 16

---
+++ day15.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function (fn, t) {
+    let id;
+    return function (...args) {
+
+        clearTimeout(id); // we can pass undefined here with no error
+        id = setTimeout(() => {
+            fn(...args);
+        }, t);
+    }
+};
+
+// Here the solution is very simple such that we clear all the timeout ids, but in actual scenario only those will be cleared whose setTimeout is not executed yet.
+// Clearning ids of those setTimeouts which are already executed will not have any effect.
+
+
+const log = debounce(console.log, 100);
+log('Hello'); // cancelled
+log('Hello'); // cancelledlog('Hello'); // Logged at t=100ms
---
+++ day16.js
@@ -0,0 +1,48 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var throttle = function (fn, t) {
+
+    // Throttled state - when the current function is in setTimeout.
+    let isThrottled = false;
+
+    // undefined when there is no scheduled call.
+    let nextArgs;
+
+    return function (...args) {
+
+        if (isThrottled) {
+
+            // function is already in throttle and more calls are scheduled.
+            nextArgs = args;
+        } else {
+
+            fn(...args);
+            isThrottled = true;
+
+            // no more calls are scheduled. handling unscheduled cases.
+            setTimeout(recursiveCalling, t);
+        }
+
+        function recursiveCalling() {
+
+            if (nextArgs) {
+                fn(...nextArgs);
+                isThottled = true;
+                nextArgs = null;
+                setTimeout(recursiveCalling, t);
+
+            } else {
+                isThrottled = false;
+            }
+        }
+    }
+};
+
+// Unlike debounce, throttle will execute the function at least once in the given time interval.
+
+const throttled = throttle(console.log, 100);
+throttled("log"); // logged immediately.
+throttled("log"); // logged at t=100ms.
